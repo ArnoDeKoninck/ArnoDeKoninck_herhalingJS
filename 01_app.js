15903b58250a3ec6cb609d1fe4c89d930ce3e75c
Arno De Koninck <arnodekoninck.contact@gmail.com>
2020-03-02 09:42:42 +0100

    Oefening 1 klaar

---
+++ 01_app.js
@@ -0,0 +1,33 @@
+//Declarations
+
+let opleiding = "Webontwikkelaar";
+let vak1 = "html&css";
+let vak2 = "JavaScript";
+let vak3 = "WordPress";
+let vak4 = "BootStrap";
+
+// VakkenArray?
+
+document.write(
+  "<p>Ik volg de opleiding " +
+    '"' +
+    opleiding +
+    '"' +
+    ". We krijgen ondermeer volgende vakken: " +
+    '"' +
+    vak1 +
+    '"' +
+    ", " +
+    '"' +
+    vak2 +
+    '"' +
+    ", " +
+    '"' +
+    vak3 +
+    '"' +
+    " en " +
+    '"' +
+    vak4 +
+    '"' +
+    "."
+);
