a6b6a1e95ad97c383ff905a55f751163c2de3099
Arno De Koninck <arnodekoninck.contact@gmail.com>
2020-03-02 10:22:08 +0100

    Oefening 3 klaar

---
+++ 03_app.js
@@ -0,0 +1,14 @@
+function toonWinnaars() {
+  let winnersArr = ["Frankrijk", "Kroatië", "België"];
+
+  document.write(
+    "<p>Op plaats 1 staat: " +
+      winnersArr[0] +
+      ". <br><br>Op plaats 2 staat: " +
+      winnersArr[1] +
+      ".<br><br>Op plaats 3 staat: " +
+      winnersArr[2] +
+      "."
+  );
+}
+toonWinnaars();
