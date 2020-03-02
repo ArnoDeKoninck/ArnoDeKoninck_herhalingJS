7720064329e11c11215fc28c975b2a8e25572fb6
Arno De Koninck <arnodekoninck.contact@gmail.com>
2020-03-02 11:43:11 +0100

    Opdracht 5 klaar

--- 05_app.js
+++ 05_app.js
@@ -1,10 +1,11 @@
 let txt = "Godslastering en andere vuile woorden zullen gecensureerd worden!";
 
 let input = prompt("Welk woord wil je censureren?");
-
+document.write(txt + "<br>");
 if (txt.includes(input)) {
   document.write(
-    txt.substring(0, txt.search(input)) +
+    "<br>" +
+      txt.substring(0, txt.search(input)) +
       " GECENSUREERD " +
       txt.substring(txt.search(input) + input.length, txt.length)
   );
